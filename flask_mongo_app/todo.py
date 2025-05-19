4b2f96530eba721be14b08cc70947ed2f155a033
Luser68 <akshay.lonkar619@gmail.com>
2025-05-19 18:02:15 +0530

    Add Item UUID field to the To-Do form

---
+++ flask_mongo_app/todo.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify, render_template
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://Test_app:kBV30N2iBv6KEeni@tutedude-project.madp0ix.mongodb.net/")
+db = client['tutedude']
+collection = db['todos']
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('todo.html')
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    item_name = request.form['item_name']
+    item_description = request.form['item_description']
+
+    todo_item = {
+        'item_name': item_name,
+        'item_description': item_description
+    }
+
+    try:
+        collection.insert_one(todo_item)
+        return jsonify({"message": "To-Do item added successfully!"}), 201
+    except Exception as e:
+        return jsonify({"error": f"Error saving to database: {str(e)}"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
